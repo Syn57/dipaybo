86079a749330e4a31df989a68084efb631276e5f
Syn57 <luthfi.n57@gmail.com>
2023-09-10 20:01:29 +0700

    finishing home, vehicle, and profile layout

--- app/src/main/java/com/pkm/dipaybo/MainActivity.kt
+++ app/src/main/java/com/pkm/dipaybo/MainActivity.kt
@@ -3,7 +3,12 @@ package com.pkm.dipaybo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.pkm.dipaybo.databinding.ActivityMainBinding
@@ -17,11 +22,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = Firebase.auth
         val firebaseUser = auth.currentUser
+        supportActionBar?.hide()
 
         if (firebaseUser == null) {
             // Not signed in, launch the Login activity
@@ -29,6 +36,20 @@ class MainActivity : AppCompatActivity() {
             finish()
             return
         }
+        val navView: BottomNavigationView = binding.bnHome
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_vehicle,
+                R.id.nav_parking,
+                R.id.nav_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
     }
 }
